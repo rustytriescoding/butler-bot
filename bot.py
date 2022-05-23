0268bfe83bd63cf3bb08fcd430b89d9fa3462f82
Russell T <russelltang88@gmail.com>
2022-05-22 21:20:57 -0400

    edited rank code

--- bot.py
+++ bot.py
@@ -46,16 +46,15 @@ async def valrank(ctx, *,username: str):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
         response = requests.get("https://api.henrikdev.xyz/valorant/v1/mmr/na/{}/{}".format(user[0], user[1]), headers=headers)
 
-        data = response.json()       
+        data = response.json()      
 
-
-        await ctx.send("Rank: " + data["data"]["currenttierpatched"])
-        # await ctx.send("Current Elo: " + data["data"]["ranking_in_tier"])
-        # await ctx.send("Last Match Elo: " + data["data"]["mmr_change_to_last_game"])
+        
+        await ctx.send("Rank: " + data['data']['currenttierpatched'])
+        # await ctx.send(data['data']['ranking_in_tier'])
+        # await ctx.send(data['data']['mmr_change_to_last_game'])
     except:
         await ctx.send("ERROR")
-    
-    
+
 
 
 
