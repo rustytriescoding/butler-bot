a2659738ac7370af2ee4a4c414f5416344c7aa6b
Johnny Ling <johnnyling77@gmail.com>
2022-05-22 22:15:27 -0400

    Adding Embeds to Status Command

--- bot.py
+++ bot.py
@@ -7,20 +7,53 @@ load_dotenv()
 
 intents = discord.Intents.default()
 intents.members = True
-client = commands.Bot(command_prefix = "?", intents=intents)
+intents.presences = True
+bot = commands.Bot(command_prefix = "?", intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print('Logged in as', client)
+    print('Logged in as', bot)
 
-@client.command()
+@bot.event
+async def on_ready():
+    # userID = 338492851040157696
+    # channelID = 831017916354920468
+    userID = 235088799074484224
+    channelID = 977787584178708480
+    channel = bot.get_channel(channelID)
+    print(bot.user.name + ' is now online')
+    for i in range (5):
+        await channel.send('<@{}>'.format(userID) + ' Baldar Butler at your service!')
+    await bot.change_presence(
+        status = discord.Status.online,											                # Status: online, idle, dnd, invisible
+        activity = discord.Game('Baldar Butler at your Service!'),
+    )
+
+@bot.command()
 async def status(ctx):
-    membersList = []
-    for guild in client.guilds:
-        for member in guild.members:
-            print(member)
-            membersList.append(member)
-    print(membersList)
-    print(membersList[0])
-                
-client.run(os.getenv("DISCORD_TOKEN"))
+    categories = ["Total Members:", "Total Bots:", "Online:", "Offline:"]
+    memberCount = 0
+    botCount = 0
+    onlineMembers = 0
+    offlineMembers = 0
+
+    for guild in bot.guilds:
+        for user in guild.members:
+            if (user.bot == False):
+                if (user.status != discord.Status.offline):
+                    onlineMembers += 1
+                else:
+                    offlineMembers += 1
+                memberCount += 1
+            else:
+                botCount += 1
+
+    embed = discord.Embed(title="New Age Server Status", color = 0xf04e1f)
+
+    # await ctx.channel.send("{} {}".format(categories[0], memberCount))
+    # await ctx.channel.send("{} {}".format(categories[1], botCount))
+    # await ctx.channel.send("{} {}".format(categories[2], onlineMembers))
+    # await ctx.channel.send("{} {}".format(categories[3], offlineMembers))
+    await ctx.channel.send(embed)
+
+bot.run(os.getenv("DISCORD_TOKEN"))
