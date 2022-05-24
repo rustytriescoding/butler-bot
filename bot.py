8e3e8605503e0f3be1a6811b9a54c70f546bc447
Johnny Ling <johnnyling77@gmail.com>
2022-05-24 05:06:23 -0400

    Created Embeds for Valorant Feature

--- bot.py
+++ bot.py
@@ -44,66 +44,55 @@ async def skull(ctx, userinput):
 # 1. Storing user data into a database / 
 @bot.command()
 async def valrank(ctx, *,username: str):
-    # try:
-    user = username.split("#")
-    # print(user)
+    try:
+        user = username.split("#")
+        # print(user)
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
-    response = requests.get("https://api.henrikdev.xyz/valorant/v1/mmr/na/{}/{}".format(user[0], user[1]), headers=headers)
-    response2 = requests.get("https://valorant-api.com/v1/competitivetiers", headers=headers)
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+        response = requests.get("https://api.henrikdev.xyz/valorant/v1/mmr/na/{}/{}".format(user[0], user[1]), headers=headers)
+        response2 = requests.get("https://valorant-api.com/v1/competitivetiers", headers=headers)
 
-    data = response.json()
-    data2 = response2.json()
+        data = response.json()
+        data2 = response2.json()
 
-    # categories = ["Rank", "Elo", "Last Match's Elo"]
-    # values = [str(data['data']['currenttierpatched']), str(data['data']['ranking_in_tier']), str(data['data']['mmr_change_to_last_game'])]
+        # categories = ["Rank", "Elo", "Last Match's Elo"]
+        # values = [str(data['data']['currenttierpatched']), str(data['data']['ranking_in_tier']), str(data['data']['mmr_change_to_last_game'])]
 
-    c = 0
-    embed = discord.Embed()
-    
-    # Retrieves latest patch of data and stores it locally (this way it runs faster without having to keep requesting from server)
-    if (len(dataDict['rankNames']) <= 0):
+        c = 0
+        embed = discord.Embed()
+        
+        # Retrieves latest patch of data and stores it locally (this way it runs faster without having to keep requesting from server)
+        if (len(dataDict['rankNames']) <= 0):
 
-        dataDict['categories'] = ["Rank", "Elo", "Last Match's Elo"]
-        dataDict['values'] = [str(data['data']['currenttierpatched']), str(data['data']['ranking_in_tier']), str(data['data']['mmr_change_to_last_game'])]
+            dataDict['categories'] = ["Rank", "Elo", "Last Match's Elo"]
+            dataDict['values'] = [str(data['data']['currenttierpatched']), str(data['data']['ranking_in_tier']), str(data['data']['mmr_change_to_last_game'])]
 
-        for i in data2['data'][-1]['tiers']:
-            print(i)
-            if ((type(i['tierName']) == str) and (type(i['largeIcon']) == str) and (type(i['color']) == str)):
-                print(i['largeIcon'])
-                print(i['color'])
-                dataDict['rankNames'].append(i['tierName'])
-                dataDict['rankImgs'].append(i['largeIcon'])
-                dataDict['rankColors'].append(i['color'])
-            else:
-                continue
-            print("\n")
+            for i in data2['data'][-1]['tiers']:
+                print(i)
+                if ((type(i['tierName']) == str) and (type(i['largeIcon']) == str) and (type(i['color']) == str)):
+                    print(i['largeIcon'])
+                    print(i['color'])
+                    dataDict['rankNames'].append(i['tierName'])
+                    dataDict['rankImgs'].append(i['largeIcon'])
+                    dataDict['rankColors'].append(i['color'])
+                else:
+                    continue
+                print("\n")
 
-    embed.title = "{}'s Ranked Info".format(user[0])
-    embed.timestamp = datetime.datetime.utcnow()
-    embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
-    embed.add_field(name=dataDict['categories'][0], value=dataDict['values'][0], inline=False)
-    embed.add_field(name=dataDict['categories'][1], value=dataDict['values'][1], inline=False)
-    embed.add_field(name=dataDict['categories'][2], value=dataDict['values'][2], inline=False)
+        embed.title = "{}'s Ranked Info".format(user[0])
+        embed.timestamp = datetime.datetime.utcnow()
+        embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
+        embed.add_field(name=dataDict['categories'][0], value=dataDict['values'][0], inline=False)
+        embed.add_field(name=dataDict['categories'][1], value=dataDict['values'][1], inline=False)
+        embed.add_field(name=dataDict['categories'][2], value=dataDict['values'][2], inline=False)
 
-    for i in range(len(dataDict['rankImgs'])):
-        # print(i)
-        # print(dataDict['rankNames'][i])
-        # print(dataDict['rankImgs'][i])
-        # print(dataDict['color'][i])
-        if (dataDict['values'][0].lower() == dataDict['rankNames'][i].lower()):
-            embed.color = int("0x" + str(dataDict['rankColors'][i][:6]), 16)
-            embed.set_thumbnail(url=dataDict['rankImgs'][i])
+        for i in range(len(dataDict['rankImgs'])):
+            if (dataDict['values'][0].lower() == dataDict['rankNames'][i].lower()):
+                embed.color = int("0x" + str(dataDict['rankColors'][i][:6]), 16)
+                embed.set_thumbnail(url=dataDict['rankImgs'][i])
 
-    # print(data2)
-
-
-    await ctx.send(embed=embed)
-
-    # await ctx.send("Rank: " + str(data['data']['currenttierpatched']))
-    # await ctx.send("Elo: " + str(data['data']['ranking_in_tier']))
-    # await ctx.send("Last Match Elo: "+ str(data['data']['mmr_change_to_last_game']))
-    # except:
-    #     await ctx.send("ERROR")
+        await ctx.send(embed=embed)
+    except:
+        await ctx.send("ERROR")
 
 bot.run(os.getenv("DISCORD_TOKEN"))
