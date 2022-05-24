23e2e9e250dcbabe13b38074c439929d8308b5f2
Johnny Ling <johnnyling77@gmail.com>
2022-05-24 14:31:45 -0400

    Adding Feature to Retrieve Content from Valorant API

--- bot.py
+++ bot.py
@@ -17,6 +17,8 @@ dataDict = {
              'rankColors' : []
            }
 
+valContent = {}
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
@@ -28,6 +30,7 @@ async def on_command_error(ctx, error):
         print(error)
         await ctx.send(error)
     else:
+        print(error)
         await ctx.send("There is an error with your command")
 
 @bot.command()
@@ -51,27 +54,35 @@ async def skull(ctx, userinput):
 
 # Make the bot faster at loading ranked info by:
 # 1. Storing user data into a database / 
+# Only call to make a request for valContent if the database / locally stored data is there are any changes in data
 @bot.command()
 async def valrank(ctx, *,username: str):
-    try:
+    # try:
         user = username.split("#")
+        locale = "en-US"
 
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
-        response = requests.get("https://api.henrikdev.xyz/valorant/v1/mmr/na/{}/{}".format(user[0], user[1]), headers=headers)
-        response2 = requests.get("https://valorant-api.com/v1/competitivetiers", headers=headers)
+        playerInfo = requests.get("https://api.henrikdev.xyz/valorant/v1/mmr/na/{}/{}".format(user[0], user[1]), headers=headers)
+        ranks = requests.get("https://valorant-api.com/v1/competitivetiers", headers=headers)
+        valContent = requests.get("https://na.api.riotgames.com/val/content/v1/contents?locale={}&api_key={}".format(locale, os.getenv("VAL_API_KEY")))
+        # leaderboards = requests.get("", headers=headers)
+        # response4 = requests.get("https://dgxfkpkb4zk5c.cloudfront.net/leaderboards/affinity/{region}/queue/competitive/act/{actId}?startIndex={startIndex}&size={size}".format("NA", ""))
+
+
 
         print("Retrieving {}'s Ranked Stats...".format(user[0]))
 
-        data = response.json()
-        data2 = response2.json()
+        data = playerInfo.json()
+        data2 = ranks.json()
+        data3 = valContent.json()
+        # data4 = response4.json()
+
+        for x in data3:
+            print(x)
+        # print(data4)
 
         c = 0
         embed = discord.Embed()
-        
-
-
-        
-
 
         dataDict['categories'] = ["Rank", "Elo", "Last Match's Elo"]
         dataDict['values'] = [str(data['data']['currenttierpatched']), str(data['data']['ranking_in_tier']), str(data['data']['mmr_change_to_last_game'])]
@@ -94,9 +105,9 @@ async def valrank(ctx, *,username: str):
         embed.title = "{}'s Ranked Stats".format(user[0])
         embed.timestamp = datetime.datetime.utcnow()
         embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
-        embed.add_field(name=dataDict['categories'][0], value=dataDict['values'][0], inline=False)
-        embed.add_field(name=dataDict['categories'][1], value=dataDict['values'][1], inline=False)
-        embed.add_field(name=dataDict['categories'][2], value=dataDict['values'][2], inline=False)
+        embed.add_field(name=dataDict['categories'][0], value="{}".format(dataDict['values'][0]), inline=False)
+        embed.add_field(name=dataDict['categories'][1], value="{} rr".format(dataDict['values'][1]), inline=False)
+        embed.add_field(name=dataDict['categories'][2], value="{} rr".format(dataDict['values'][2]), inline=False)
 
         for i in range(len(dataDict['rankImgs'])):
             if (dataDict['values'][0].lower() == dataDict['rankNames'][i].lower()):
@@ -105,7 +116,7 @@ async def valrank(ctx, *,username: str):
 
         await ctx.send(embed=embed)
         print("Successfully retrieved {}'s Stats!\n".format(user[0]))
-    except:
-        await ctx.send("ERROR")
+    # except:
+    #     await ctx.send("ERROR")
 
 bot.run(os.getenv("DISCORD_TOKEN"))
