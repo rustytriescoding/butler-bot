0c63c7072df14d436dafc9e76bea233fc6a4db5f
Russell T <russelltang88@gmail.com>
2022-05-31 18:02:28 -0400

    neon graph

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ from pymongo import MongoClient
 import re
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import external_functions as EF
 
 load_dotenv()
@@ -64,10 +65,56 @@ async def on_command_error(ctx, error):
         await ctx.send("There is an error with your command")
 
 @bot.command()
-async def plottest(ctx, *args):
-    x = args
-    
-    plt.bar(np.arange(len(x)), x)
+async def plottest(ctx):
+    plt.style.use("dark_background")
+
+    for param in ['text.color', 'axes.labelcolor', 'xtick.color', 'ytick.color']:
+        plt.rcParams[param] = '0.9'  # very light grey
+
+    for param in ['figure.facecolor', 'axes.facecolor', 'savefig.facecolor']:
+        plt.rcParams[param] = '#212946'  # bluish dark grey
+
+    colors = [
+        '#08F7FE',  # teal/cyan
+        '#FE53BB',  # pink
+        '#F5D300',  # yellow
+        '#00ff41',  # matrix green
+    ]
+
+
+    df = pd.DataFrame({'A': [1, 3, 9, 5, 2, 1, 1],
+                    'B': [4, 5, 5, 7, 9, 8, 6]})
+
+    fig, ax = plt.subplots()
+
+    df.plot(marker='o', color=colors, ax=ax)
+
+    # Redraw the data with low alpha and slighty increased linewidth:
+    n_shades = 10
+    diff_linewidth = 1.05
+    alpha_value = 0.3 / n_shades
+
+    for n in range(1, n_shades+1):
+
+        df.plot(marker='o',
+                linewidth=2+(diff_linewidth*n),
+                alpha=alpha_value,
+                legend=False,
+                ax=ax,
+                color=colors)
+
+    # Color the areas below the lines:
+    for column, color in zip(df, colors):
+        ax.fill_between(x=df.index,
+                        y1=df[column].values,
+                        y2=[0] * len(df),
+                        color=color,
+                        alpha=0.1)
+
+    ax.grid(color='#2A3459')
+
+    ax.set_xlim([ax.get_xlim()[0] - 0.2, ax.get_xlim()[1] + 0.2])  # to not have the markers cut off
+    ax.set_ylim(0)
     plt.savefig("test.png")
     plt.close()
     image = discord.File("test.png")
